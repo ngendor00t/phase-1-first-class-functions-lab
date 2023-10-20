d832885d6d7d12f000eb0f889b4e060dd3852722
Reginawambui <ngendoanita8@gmail.com>
2023-10-20 08:48:18 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2);
+    }
+    
+    const returnLastTwoDrivers = function(drivers){
+        return drivers.slice(-2);
+    }
+    
+    let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+    
+    function createFareMultiplier(fare){
+        return function(multiplier){
+            return fare * multiplier;
+        }
+    }
+    
+    const fareDoubler = function(createFareMultiplier){
+        return createFareMultiplier * 2;
+    }
+    
+    const fareTripler = function(createFareMultiplier){
+        return createFareMultiplier * 3;
+    }
+    
+    function selectDifferentDrivers(drivers, selectingDrivers){
+        return selectingDrivers(drivers);
+    }
